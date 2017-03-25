d592b4f1c85e78963787cfc4188275afaa13d816
David Chmura <dschmura@humbledaisy.com>
2017-03-24 20:04:06 -0400

    UPDATED: server configs

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,19 +1,16 @@
-# config valid only for current version of Capistrano
-lock "3.8.0"
-
-# Change these as appropriate
-set :application, 'dschmura_website'
-set :user,        'deployer'
-set :repo_url,    'git@github.com:dschmura/dschmura_website.git'
-set :puma_threads, [4, 16]
-set :puma_workers,  0
+set :repo_url,        'git@github.com:dschmura/dschmura_website.git'
+set :application,     'dschmura_website'
+set :user,            'deploy'
+set :puma_threads,    [4, 16]
+set :puma_workers,    0
 
 # Don't change these unless you know what you're doing
+
 set :pty,             true
 set :use_sudo,        false
 set :stage,           :production
 set :deploy_via,      :remote_cache
-set :deploy_to,       "/home/#{fetch(:user)}/apps/#{fetch(:application)}"
+set :deploy_to,       "/home/#{fetch(:user)}/#{fetch(:application)}"
 set :puma_bind,       "unix://#{shared_path}/tmp/sockets/#{fetch(:application)}-puma.sock"
 set :puma_state,      "#{shared_path}/tmp/pids/puma.state"
 set :puma_pid,        "#{shared_path}/tmp/pids/puma.pid"
@@ -22,22 +19,28 @@ set :puma_error_log,  "#{release_path}/log/puma.access.log"
 set :ssh_options,     { forward_agent: true, user: fetch(:user), keys: %w(~/.ssh/id_rsa.pub) }
 set :puma_preload_app, true
 set :puma_worker_timeout, nil
-set :puma_init_active_record, false  # Change to true if using ActiveRecord
-set :ssh_options,     { forward_agent: true, user: fetch(:user), keys: %w(~/.ssh/id_rsa.pub) }
+set :puma_init_active_record, true  # Change to false when not using ActiveRecord
 
-append :linked_files,  "config/secrets.yml.key"
-append :linked_dirs, "log", "tmp/pids", "tmp/cache", "tmp/sockets", "vendor/bundle", "public/system", "public/uploads"
+
 
 ## Defaults:
+
+
 # set :scm,           :git
+
 # set :branch,        :master
+
 # set :format,        :pretty
+
 # set :log_level,     :debug
+
 # set :keep_releases, 5
 
+
 ## Linked Files & Directories (Default None):
-# set :linked_files, %w{config/database.yml}
-# set :linked_dirs,  %w{bin log tmp/pids tmp/cache tmp/sockets vendor/bundle public/system}
+
+set :linked_files, %w{config/database.yml config/secrets.yml}
+set :linked_dirs,  %w{bin log tmp/pids tmp/cache tmp/sockets vendor/bundle public/system}
 
 namespace :puma do
   desc 'Create Directories for Puma Pids and Socket'
@@ -48,7 +51,7 @@ namespace :puma do
     end
   end
 
-  before :start, :make_dirs
+    before :start, :make_dirs
 end
 
 namespace :deploy do
@@ -58,33 +61,53 @@ namespace :deploy do
       unless `git rev-parse HEAD` == `git rev-parse origin/master`
         puts "WARNING: HEAD is not the same as origin/master"
         puts "Run `git push` to sync changes."
-        exit
+      exit
+    end
+  end
+end
+
+desc 'Initial Deploy'
+  task :initial do
+  on roles(:app) do
+    before 'deploy:restart', 'puma:start'
+    invoke 'deploy'
+  end
+end
+
+desc 'Restart application'
+  task :restart do
+    on roles(:app), in: :sequence, wait: 5 do
+    invoke 'puma:restart'
+  end
+end
+
+desc 'Upload to shared/config'
+  task :upload do
+  on roles (:app) do
+    upload! "config/database.yml", "#{shared_path}/config/database.yml"
+    upload! "config/secrets.yml",  "#{shared_path}/config/secrets.yml"
+  end
+end
+
+before :starting,  :check_revision
+after  :finishing, :compile_assets
+after  :finishing, :cleanup
+after  :finishing, :restart
+end
+
+desc "Run rake db:seed on a remote server."
+task :seed do
+  on roles (:app) do
+    within release_path do
+      with rails_env: fetch(:rails_env) do
+        execute :rake, "db:seed"
       end
     end
   end
-
-  desc 'Initial Deploy'
-  task :initial do
-    on roles(:app) do
-      before 'deploy:restart', 'puma:start'
-      invoke 'deploy'
-    end
-  end
-
-  desc 'Restart application'
-  task :restart do
-    on roles(:app), in: :sequence, wait: 5 do
-      invoke 'puma:restart'
-    end
-  end
-
-  before :starting,     :check_revision
-  after  :finishing,    :compile_assets
-  after  :finishing,    :cleanup
-  after  :finishing,    :restart
 end
 
 # ps aux | grep puma    # Get puma pid
-# kill -s SIGUSR2 pid   # Restart puma
-# kill -s SIGTERM pid   # Stop puma
 
+# kill -s SIGUSR2 pid   # Restart puma
+
+# kill -s SIGTERM pid   # Stop puma
