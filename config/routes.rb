d90e1449e43b4d9ce4022d2db2c9cefe466755a4
David Chmura <dschmura@humbledaisy.com>
2017-12-31 10:38:56 -0500

    UPDATE: Routes to remove unneeded pages/ prefix

--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,12 @@
 Rails.application.routes.draw do
   root to: 'pages#index'
-  get 'pages/index'
-
-  get 'pages/about'
-
-  get 'pages/contact'
-
-  get 'pages/privacy'
+  get '/about', to: 'pages#about'
+  get '/contact', to: 'pages#contact'
+  get '/privacy', to: 'pages#privacy'
 
   match '/resume', :to => 'resume#index', via: :get
+  get 'contact', to: 'contact_messages#new', as: 'new_contact_message'
+  post 'contact', to: 'contact_messages#create', as: 'create_contact_message'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
