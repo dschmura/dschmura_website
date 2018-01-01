b05e0e167eac2aad62b5e0a846b9cb83e1cef899
David Chmura <dschmura@humbledaisy.com>
2018-01-01 16:43:38 -0500

    ADD: create feedback object for each page.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,11 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+  before_action :create_feedback
+
+  private
+  
+  def create_feedback
+    @feedback = Feedback.new
+  end
+
 end
